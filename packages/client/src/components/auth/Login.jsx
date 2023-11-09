aa09163ab5df0643bda014e0f3a810dda2ce68e3
amitg01 <amitg.ghy@gmail.com>
2023-11-09 15:25:18 +0530

    prod deployment changes

--- packages/client/src/components/auth/Login.jsx
+++ packages/client/src/components/auth/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
       onSubmit={(values, actions) => {
         const vals = { ...values };
         actions.resetForm();
-        fetch("http://localhost:4000/auth/login", {
+        fetch(`${import.meta.env.VITE_REACT_APP_SERVER_URL}/auth/login`, {
           method: "POST",
           credentials: "include",
           headers: {
